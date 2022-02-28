68eef7c54f2d56f489fce772679b3ccbcc996e72
Vineta-K <91382171+Vineta-K@users.noreply.github.com>
2022-02-28 10:18:47 +0000

    Update test_flashloan.py
    change for updated fcn names

--- tests/test_flashloan.py
+++ tests/test_flashloan.py
@@ -5,20 +5,18 @@ from brownie import (
 
 import pytest
 
-lender_address = "0xC22F01ddc8010Ee05574028528614634684EC29e" #
-
+lender_address = "0xC22F01ddc8010Ee05574028528614634684EC29e" #jAVAX
 token = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7" #wAVAX
 
 amount = 10
 
 @pytest.fixture
-def liq():
+def liquidatooor():
     account = accounts[0]
     JoeTroller_address = "0xdc13687554205E5b89Ac783db14bb5bba4A1eDaC"
     return account.deploy(Liquidatooor,JoeTroller_address)
 
-def test_flash(liq):
+def test_flash(liquidatooor):
     account = accounts[0]
-
-    result = liq.doFlashLoan(lender_address,token,amount,{'from':account})
+    result = liquidatoor.liquidateWithFlashLoan(lender_address,token,amount,{'from':account})
     print(result)
